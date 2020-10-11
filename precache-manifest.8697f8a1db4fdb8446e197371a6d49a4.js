d012bf169c305726898f79e9bf384d4a8598a210
varun08developer <varun8designer@gmail.com>
2020-10-11 17:48:44 +0530

    Updates

---
+++ precache-manifest.8697f8a1db4fdb8446e197371a6d49a4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f419aa5e6eec93bbb5d2aec56233ce",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "de37523d64c85a28e8e2",
+    "url": "/reactlivewebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "52f4eae4b6275b0e3af6",
+    "url": "/reactlivewebsite/static/css/main.f66eab50.chunk.css"
+  },
+  {
+    "revision": "de37523d64c85a28e8e2",
+    "url": "/reactlivewebsite/static/js/2.fa5b5a6b.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactlivewebsite/static/js/2.fa5b5a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f4eae4b6275b0e3af6",
+    "url": "/reactlivewebsite/static/js/main.049526f1.chunk.js"
+  },
+  {
+    "revision": "221b0b4f849c81e12510",
+    "url": "/reactlivewebsite/static/js/runtime-main.2d79b22d.js"
+  },
+  {
+    "revision": "a4e21202756d42849a6037081132e5d6",
+    "url": "/reactlivewebsite/static/media/about.a4e21202.png"
+  },
+  {
+    "revision": "be9dda907b0dd518a0325a70b68ba69c",
+    "url": "/reactlivewebsite/static/media/android.be9dda90.jpg"
+  },
+  {
+    "revision": "c68903389913d2b1d3d1d37a71dde3ea",
+    "url": "/reactlivewebsite/static/media/app.c6890338.jpg"
+  },
+  {
+    "revision": "2a88af2d345a8c744d6aa34d77c9cd35",
+    "url": "/reactlivewebsite/static/media/img2.2a88af2d.png"
+  },
+  {
+    "revision": "f6fec884926232e95a2ddbaf87944dcc",
+    "url": "/reactlivewebsite/static/media/marketing.f6fec884.jpg"
+  },
+  {
+    "revision": "ffe6b6b3a757d27df8975eeb40f70727",
+    "url": "/reactlivewebsite/static/media/web.ffe6b6b3.jpg"
+  }
+]);
